2122258238748ff9a5290df792ba193ef1c66803
nesto <nescanpac@alu.edu.gva.es>
2024-01-17 19:54:13 +0100

    sdfgdf

--- src/main/java/com/example/ApiCiutatsNestor/CiutatCrontroller.java
+++ src/main/java/com/example/ApiCiutatsNestor/CiutatCrontroller.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.parser.Entity;
+
 import java.util.List;
 import java.util.Optional;
 
